24cd9cbddf558a279537373d5a02e8750ed4806c
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-05-09 19:54:01 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,6 +12,11 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
   def create
     Tweet.create(tweet_params)
   end
