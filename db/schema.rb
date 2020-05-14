612a0d4f88f2940be3c54441f505fb0bec0bc8a5
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-05-14 21:18:16 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_091314) do
+ActiveRecord::Schema.define(version: 2020_05_14_121758) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
